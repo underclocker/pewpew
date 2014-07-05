3fd422bca9f1e129b9e9e8d8ec415950f238da37
underclocker <underclocker@gmail.com>
2014-07-04 22:32:30 -0700

    Removed files

--- core/src/com/pwnscone/pewpew/util/Util.java
+++
@@ -1,9 +0,0 @@
-package com.pwnscone.pewpew.util;
-
-import com.badlogic.gdx.math.Vector3;
-
-public class Util {
-	public static float RAD_TO_DEG = (float) (180.0 / Math.PI);
-	public static float DEG_TO_RAD = (float) (Math.PI / 180.0);
-	public static Vector3 T1 = new Vector3();
-}
